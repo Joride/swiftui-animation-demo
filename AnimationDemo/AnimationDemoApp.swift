f86dda15488401362ce16ba28709dec7e15f0914
Joride <kerrelinc@gmail.com>
2024-04-29 15:20:42 +0200

    Initial Commit

---
+++ AnimationDemo/AnimationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationDemoApp.swift
+//  AnimationDemo
+//
+//  Created by Jorrit van Asselt on 29/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
